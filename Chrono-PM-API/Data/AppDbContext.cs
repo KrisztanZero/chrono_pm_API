b87705fd6d55cd9d8a3acad68183771d38ae2b24
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-02-27 13:35:01 +0100

    Create user authentication

---
+++ Chrono-PM-API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Chrono_PM_API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chrono_PM_API.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+    {
+        
+    }
+}
