78a9fe3bea300114da082aad4049055e968fa3e9
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-02-28 12:45:32 +0100

    Add authorization

--- Chrono-PM-API/Program.cs
+++ Chrono-PM-API/Program.cs
@@ -3,12 +3,24 @@ using Chrono_PM_API.Models;
 using Chrono_PM_API.Repositories;
 using Chrono_PM_API.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder
@@ -36,6 +48,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
