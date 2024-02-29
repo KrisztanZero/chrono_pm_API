dd3b63f6d8cdb8418a68c547c586c957b5d6bd83
KrisztanZero <s.molnar.krisztian@gmail.com>
2024-02-29 18:35:06 +0100

    Create UserUtilities.cs

---
+++ Chrono-PM-API/Utilities/UserUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chrono_PM_API.Utilities;
+
+public static class UserUtilities
+{
+    public static string GetCurrentUserId(ControllerBase controller)
+    {
+        var currentUserId = controller.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrEmpty(currentUserId))
+        {
+            throw new ArgumentException("Invalid or missing user identifier.", nameof(currentUserId));
+        }
+        return currentUserId;
+    }
+    
+}
